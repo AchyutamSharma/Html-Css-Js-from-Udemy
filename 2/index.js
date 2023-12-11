bbb75a636b820f4143b8595043526d778d9dd2d6
Akshutam Sharma <111337963+AchyutamSharma@users.noreply.github.com>
2023-12-11 23:09:45 +0530

    Add files via upload

---
+++ 2/index.js
@@ -0,0 +1,18 @@
+//alert("Hello!! \n Welcome to Our Page.");
+
+
+function getAnsHere(){
+const bodys = document.querySelector(".headingdetail");
+console.dir(bodys);
+console.log(bodys.tagName); // it print the tag Name in console
+console.log(bodys.textContent); // it print the text of (.headingdetail) class/bodys on the console.
+}
+
+const allList = document.querySelector(".list");
+console.dir(allList);
+console.log(allList.innerText);
+console.log(allList.innerHTML);
+
+const firstbtn = document.querySelector("#my-btn");
+console.dir(firstbtn);
+
