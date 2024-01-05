8481dc3e90505a0cdf109792edaa9b234f62bfae
= <achyutamgrd@gmail.com>
2024-01-05 12:41:19 +0530

    New One

---
+++ 7/index.js
@@ -0,0 +1,20 @@
+let btn = document.querySelector("#btn-1");
+
+btn.ondblclick = (event) =>{
+    console.log(event);
+    console.log(event.type);
+    console.log(event.target);
+    console.log(event.clientX, event.clientY);
+
+}
+
+let btn2 = document.querySelector("#btn-2");
+btn2.addEventListener("click",() =>{
+    console.log("This is the example of addEventListener.");
+});
+
+
+
+
+
+
