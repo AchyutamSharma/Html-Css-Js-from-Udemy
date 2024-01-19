75b21522b44d2c3059d72b89581806b082e329bd
Akshutam Sharma <111337963+AchyutamSharma@users.noreply.github.com>
2024-01-19 14:10:33 +0530

    Add files via upload

---
+++ 15-Try-Catch/style.js
@@ -0,0 +1,27 @@
+// setTimeout and Try-Catch (Error Handling)
+
+/*
+let a = 10;
+let b = 20;
+
+
+console.log("a = ",a);
+console.log("b = ",b);
+try{
+    console.log("a+b =", a+c);
+    
+}catch(err){
+    console.log(err);
+}
+console.log("a-b =", a-b);
+
+*/
+
+
+setTimeout(()=>{
+    
+    console.log("Hogaya Delay 3sec....")
+},3000);
+
+
+
